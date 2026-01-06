9f9f1b2425b7162aa84f316c9c71b3509fbe32f9
kmsoon <alstnsl00@gmail.com>
2026-01-06 13:08:47 +0900

    소스 정리

--- rabbit-mq/consumer/src/main.ts
+++ rabbit-mq/consumer/src/main.ts
@@ -11,6 +11,7 @@ function rabbitmqURL() {
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   // await app.listen(process.env.PORT ?? 3000);
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
@@ -25,7 +26,6 @@ async function bootstrap() {
       },
     },
   );
-
   await app.listen();
 }
 bootstrap();
